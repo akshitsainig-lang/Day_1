b50e46d1cf788eb2735f439cc16c33fbe628cd61
akshitsainig-lang <akshitsainig@gmail.com>
2026-08-11 23:56:28 +0530

    Add files via upload

---
+++ Day2.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    float radius;
+    scanf("%f",&radius);
+    printf("%f",3.14*radius*radius);
+    scanf("%f",&radius);
+    printf("%f",2*3.14*radius);
+
+    return 0;
+}
