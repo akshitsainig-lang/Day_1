8236f9104df86bb389e5399f384c33547da6c67e
akshitsainig-lang <akshitsainig@gmail.com>
2026-08-12 23:57:35 +0530

    Add files via upload

---
+++ Day3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main(){
+
+int a ,b , c;
+
+printf("A:");
+scanf("%d",&a);
+
+printf("B:");
+scanf("%d",&b);
+
+printf("\n BEFORE SWAPPING:\n");
+printf("THE FIRST NUMBER IS:%d\n",a);
+printf("THE SECOND NUMBER IS:%d\n",b);
+
+
+c = a;
+a = b;
+b = c;
+
+
+printf("\n AFTER SWAPPING\n");
+printf("THE FIRST LATTER IS:%d\n",a);
+printf("THE SECOND NUMBER IS:%d\n",b);
+
+return 0;
+}
+
