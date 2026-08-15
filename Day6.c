e6abeceb0ce63be54725cb29ac031ef79daf99e4
akshitsainig-lang <akshitsainig@gmail.com>
2026-08-15 23:39:18 +0530

    Add files via upload

---
+++ Day6.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+    int a;
+    printf("A:");
+    scanf("%d",&a);
+
+    if(a>0){
+        printf("POSITIVE");
+    }
+    else if(a<0){
+        printf("NEGATIVE");
+    }
+    else
+    {
+        printf("ZERO");
+    }
+
+
+}
